6eb5adb4d83b1603d36e9cad9c61073f557122c9
saiharsha009 <157323612+saiharsha009@users.noreply.github.com>
2024-01-23 13:04:56 -0500

    Add files via upload

---
+++ uncc.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>UNCC Sai Harsha</title>
+</head>
+<body>
+     <h1>Hello php this is sai harsha</h1>
+      <?php echo 9 * 13 ?>
+</body>
+</html>
